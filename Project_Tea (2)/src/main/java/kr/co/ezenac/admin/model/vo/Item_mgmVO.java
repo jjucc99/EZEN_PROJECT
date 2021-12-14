9894c1c8aaac176c5e17ea8a61d3f37817af574c
jjucc99 <59110017+jjucc99@users.noreply.github.com>
2021-12-14 17:50:33 +0900

    PROJECT_TEA
    2021-12-14

---
+++ Project_Tea (2)/src/main/java/kr/co/ezenac/admin/model/vo/Item_mgmVO.java
@@ -0,0 +1,13 @@
+package kr.co.ezenac.admin.model.vo;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Item_mgmVO {
+	private int prod_code;
+	private String inout_date;
+	private int prod_count;
+	private int item_code;
+}
