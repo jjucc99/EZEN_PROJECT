77ca9659768cb83f87e7479bef70a5daaba56440
SooY <95726028+Uoo0@users.noreply.github.com>
2022-01-06 22:45:04 +0900

    22.01.06 프로젝트 완료
    프로젝트 완료!

---
+++ Project_Tea_party/src/main/java/kr/co/ezenac/pay/model/vo/CartUpdateVO.java
@@ -0,0 +1,20 @@
+package kr.co.ezenac.pay.model.vo;
+
+public class CartUpdateVO {
+private int cart_amount;
+private int cart_item_no;
+
+public int getCart_amount() {
+	return cart_amount;
+}
+public void setCart_amount(int cart_amount) {
+	this.cart_amount = cart_amount;
+}
+public int getCart_item_no() {
+	return cart_item_no;
+}
+public void setCart_item_no(int cart_item_no) {
+	this.cart_item_no = cart_item_no;
+}
+
+}
